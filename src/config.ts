d823cc68f163c4c87cfc4d52444d0ee6e5c8853a
turtledev <turtledev65@gmail.com>
2025-08-02 22:59:45 +0300

    Add a config

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "./types";
+
+const config: Readonly<Config> = {
+    startingItems: [
+        { emoji: "💀", name: "skull" },
+        { emoji: "💧", name: "water" },
+    ],
+}
+export default config;
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,14 @@ export type Item = {
     emoji: string;
     name: string;
 };
+
+export type Combination = {
+    idx1: number;
+    idx2: number;
+    resultIdx: number;
+}
+
+export type Config = {
+    startingItems: Item[];
+    startingCombinations?: Combination[];
+};
